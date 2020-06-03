111f5f70c93dfb8544e8e6ea82425d9d52350f4e
Thrazadin <mdickey14@gmail.com>
2020-06-03 18:50:11 -0400

    Update mainwindow.h with some more comments

--- Headers/mainwindow.h
+++ Headers/mainwindow.h
@@ -1,3 +1,6 @@
+// This is a fairly straightforward MainWindow header file for a new project
+// within Qt
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -8,6 +11,8 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+// Because we will be including the plugin.h file, need to state this to prevent
+// the circular dependency issue
 class Plugin;
 
 class MainWindow : public QMainWindow
@@ -20,6 +25,8 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    // Adding a pointer to our VDJ Plugin so we can continue to interact with VDJ
     Plugin *plugin;
 };
 #endif // MAINWINDOW_H
